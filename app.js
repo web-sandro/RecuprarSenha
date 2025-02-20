2c94ccfb3f9e19d35704817c9e7abf5b0ed0d4f7
Software <sandrogomesoficial@gmail.comcd>
2025-02-20 14:50:49 -0300

    app

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 require('dotenv').config();
 app.use(session({
-  secret: process.env.session_secret,
+  secret: process.env.session_secret || 'umSegredoPadrao',
   resave: false,
   saveUninitialized: true,
 }));
